5220f5f0f30a495ad6abc819075fc0d4122e8cf4
Name <132456507+Clivkc@users.noreply.github.com>
2023-05-04 09:46:09 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+  <meta charset="UTF-8">
+  <title>YBA模组服测试官网-广告界的天花板SLT旧时代</title>
+  <meta name="keywords" content="YBA,测试,服务器,QQ群">
+  <meta name="description" content="这是YBA测试网站，我们提供游戏服务器和QQ群等服务。">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="icon" href="favicon.ico">
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<header class="header">
+  <h1>YBA测试网站</h1>
+  <nav>
+    <ul>
+      <li><button onclick="location.href='#group'">QQ群</button></li>
+      <li><button onclick="location.href='#rule'">服务器规则</button></li>
+      <li><button onclick="location.href='#wait'">建设中的网站</button></li>
+      <li><button onclick="location.href='#support'">感谢支持</button></li>
+    </ul>
+  </nav>
+</header>
+<main class="content">
+  <section id="group">
+    <h2>YBA服务器QQ群</h2>
+    <p>请勿在群内和游戏中辱骂他人。</p>
+    <p>群号：<a href="https://jq.qq.com/?_wv=1027&k=QnXHoZc3" target="_blank" rel="noopener noreferrer nofollow">191259479</a></p>
+	<a target="_blank" href="https://qm.qq.com/cgi-bin/qm/qr?k=CzD3EKocIrdy8OGFz10YUsbiwdogawYx&jump_from=webapi&authKey=SpxWoa8AW78THRRi5ghyBuNfs5xYn1ykcFig8p03+60Q6Gljkh74u1R9yBizHcTg"><img border="0" src="//pub.idqqimg.com/wpa/images/group.png" alt="『YBA服』官方交流群" title="『YBA服』官方交流群"></a>
+  </section>
+  <section id="rule">
+    <h2>服务器规则</h2>
+    <ul>
+      <li>禁止使用任何作弊软件，一经发现永久封禁IP地址</li>
+		<li>通缉最恶之人游戏ID:Cmd2,奖励永久OP最高权限</li>
+    </ul>
+  </section>
+  <section id="wait">
+    <h2>正在建设中的网站</h2>
+    <p>感谢您耐心等待。</p>
+  </section>
+  <section id="support">
+    <h2>感谢支持</h2>
+    <p>以下是一些有用的链接：</p>
+    <ul>
+      <li><button onclick="location.href='http://play.simpfun.vip:20092/'">进入YBA动态地图</button></li>
+      <li><button onclick="location.href='http://Windo.神兽.tk/客户端资源/YBA客户端.zip'">下载YBA客户端</button></li>
+      <li><button onclick="location.href='http://Winds.free1.asia/for/2B2T.html'">进入HY副站</button></li>
+		<li><button onclick="location.href='http://2B2T.99mc.asia'">进入HY官网</button></li>
+    </ul>
+  </section>
+</main>
+<footer class="footer">
+  <p>版权所有 © YBA测试网站，保留所有权利。</p>
+</footer>
+<script src="script.js"></script>
+</body>
+</html>
